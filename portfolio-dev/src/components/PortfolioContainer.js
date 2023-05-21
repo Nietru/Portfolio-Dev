5c924a367775b86494beea71bba8e752d4fd8330
Tifannie Truman <nietru143@gmail.com>
2023-05-21 15:52:56 -0400

    got react working with skeleton design

--- portfolio-dev/src/components/PortfolioContainer.js
+++ portfolio-dev/src/components/PortfolioContainer.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import NavTabs from "./NavTabs";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 
 export default function PortfolioContainer() {

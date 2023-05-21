e859784e63d6288a833f8073798367578dbda3bf
Tifannie Truman <nietru143@gmail.com>
2023-05-21 15:15:06 -0400

    added file structure and code for all components

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
